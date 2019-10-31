efd347b71a9b905c6bb0f3aa841d68540fe9eded
nikhy <nikhythanc@gmail.com>
2019-10-31 12:19:45 +0530

    Added Day 6 files

--- Day7/MyLambdaApp/slotassets/s3-bucket-setup.js
+++ Day7/MyLambdaApp/slotassets/s3-bucket-setup.js
@@ -15,7 +15,7 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Load credentials and set region from JSON file
-AWS.config.loadFromPath('./config.json');
+AWS.config.loadFromPath('../../../aws-config.json');
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
